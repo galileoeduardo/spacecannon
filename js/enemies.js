afd04b177eda9d1e007ab78e91db8c7092d6f94a
Eduardo Vieira <galileoeduardo@hotmail.com>
2020-10-03 19:07:03 -0300

    refactor enemies: launch dodge enemyHit, set config into gameobject data.list

--- js/enemies.js
+++ js/enemies.js
@@ -16,10 +16,6 @@
 
     constructor(scene) {
         this._scene = scene;
-    }
-
-    create() {
-
         for (let i = 0; i < this.config.length; i++) {
             
             let addConfig = this.config[i];
@@ -30,50 +26,48 @@
             ? { ...addConfig, start_on: 'right', start_x: 512 } 
             : { ...addConfig, start_on: 'left', start_x: -64};
 
-            this.enemies[i] = this._scene.physics.add.sprite(0,0,this.config[i].id,this.config[i].frame);
-            this.enemies[i].config = addConfig;
+            this.config[i] = addConfig;
             
         }
+    }
 
-        Phaser.Actions.SetXY(this.enemies, 0, -96, 0, -64);
+    create() {
         this.timedEnemyLaunchEvent = setInterval(this.launch, 5000);
     }
 
     update() {
 
-        this.enemies.forEach(({config,x,y,on_base}) => {
-            if (!on_base) this.enemies_actual_position.push(
-                {config,x,y}
-            );
-        });
-
-        this._scene.physics.collide(this.enemies, this._scene.Bullet.Group, this.enemyHit, this.processHit, this);
-
-        this.enemies.forEach(enemy => {
+        const enemies = this._scene.children.getAll( "name" , "enemies_ship");
+        
+        this._scene.physics.collide(enemies, this._scene.Bullet.Group, this.enemyHit, this.processHit, this);
+        
+        let same_level;
+        
+        enemies.forEach(enemy => {
             
-            const same_level = this.enemies_actual_position.find(({config,y}) => y == enemy.y && config.id != enemy.config.id);
-            
+            for (let index = 0; index < enemies.length; index++) {
+                same_level = (enemies[index].y == enemy.y && enemies[index].getData("id") != enemy.getData("id")) ? index : undefined;
+            };
+
             if (same_level != undefined) { //enemy in the same level
                 const distance = Phaser.Math.Distance.Between(
                     enemy.x,
                     enemy.y,
-                    this.enemies[same_level.config.index].x,
-                    this.enemies[same_level.config.index].y
+                    enemies[same_level].x,
+                    enemies[same_level].y
                 );
-                if (distance < 64) this.dodge(this.enemies[same_level.config.index]);
+
+                if (distance < 64) this.dodge(enemies[same_level]);
             }
 
-            if(enemy.config.on_base == false && enemy.x < -64 || enemy.x > 512) { //fora da tela
+            if(enemy.x < -64 || enemy.x > 512) { //fora da tela
                 
                 //move para faixa inferior
                 this.moveLower(enemy);
                 
             }
-
             
         });
-
-        this.enemies_actual_position = [];
 
     }
 
@@ -82,26 +76,28 @@
     }
 
     launch = () => {
-        const enemy = this.enemies[this.enemies_launch.next];
-        this.enemies_launch.next++;
-        if (enemy.config.start_on == 'left') {
-            enemy.body.velocity.x = enemy.config.force * this.speed;
+
+        const enemy = this._scene.physics.add.sprite(0,0,this.config[0].id,this.config[0].frame);
+        enemy.name = "enemies_ship";
+        enemy.setData(this.config.shift());
+
+        if (enemy.getData("start_on") == 'left') {
+            enemy.body.velocity.x = enemy.getData("force") * this.speed;
             enemy.flipX = true;
         } else {
-            enemy.body.velocity.x = (enemy.config.force * this.speed) * -1;
+            enemy.body.velocity.x = (enemy.getData("force") * this.speed) * -1;
             enemy.flipX = false;
         }
 
-        enemy.config.on_base = false;
-        enemy.x = enemy.config.start_x;
-        enemy.y = enemy.config.start_y;
+        enemy.x = enemy.getData("start_x");
+        enemy.y = enemy.getData("start_y");
 
-        if(this.enemies_launch.next >= this.enemies_launch.total) clearInterval(this.timedEnemyLaunchEvent); //stop launch
+        if(this.config.length <= 0) clearInterval(this.timedEnemyLaunchEvent); //stop launch
 
     }
 
     moveLower(enemy) {
-        enemy.x = enemy.config.start_x;
+        enemy.x = enemy.getData("start_x");
         enemy.y += 32;
     }
 
@@ -110,11 +106,11 @@
     }
 
     enemyHit(enemy) {
-        //enemy.play('hit');
+        this._scene.children.remove(enemy);
     }
 
     processHit(enemy) {
-        enemy.destroy();    
+          
     }
 
 }
